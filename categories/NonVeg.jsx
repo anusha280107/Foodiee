e7369e9991f256777233aa95ffac9d99c1126543
Anusha M <anusha28787@gmail.com>
2026-08-07 14:29:58 +0530

    Updated project

--- categories/NonVeg.jsx
+++ categories/NonVeg.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./Categories.css";
+import "./categories.css";
 
 function NonVeg() {
   const navigate = useNavigate();
--- categories/Veg.jsx
+++ categories/Veg.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./Categories.css";
+import "./categories.css";
 
 function Veg() {
   const navigate = useNavigate();
